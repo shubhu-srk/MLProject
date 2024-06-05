3a59b3c2737dd51a5b9603f8901b604554e9514b
Shubham Kshirsagar <shubhamkrs2001@gmail.com>
2024-06-05 16:05:19 +0530

    Tested Exception handling logs

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+import logger
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info() #This will give me the info about the exception in which file on which line 
@@ -17,11 +18,11 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-# if __name__ == "__main__":
+if __name__ == "__main__":
 
-#     try:
-#         a=1/0
-#     except Exception as e:
-#         logging.info("Divide by zero")
-#         raise CustomException(e,sys)
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divide by zero")
+        raise CustomException(e,sys)
     
