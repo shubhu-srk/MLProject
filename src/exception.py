68321a33fb0f166f8bb4f08448d46ad65962266c
Shubham Kshirsagar <shubhamkrs2001@gmail.com>
2024-06-06 09:58:26 +0530

    EDA and Problem Statement , Model training on Jupyter

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from logger import logging
 # import logger By importing this you can get logs of exception too
 
 def error_message_detail(error,error_detail:sys):
@@ -18,11 +18,11 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-# if __name__ == "__main__":
+if __name__ == "__main__":
 
-#     try:
-#         a=1/0
-#     except Exception as e:
-#         logging.info("Divide by zero")
-#         raise CustomException(e,sys)
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divide by zero")
+        raise CustomException(e,sys)
     
